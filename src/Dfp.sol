b025f42bab2caaaf88cb05752c61d58b6d4885da
Roman Yarlykov <roman.yarlykov@metalamp.io>
2023-06-30 18:02:59 +0600

    feat: Permit

--- src/Dfp.sol
+++ src/Dfp.sol
@@ -1,22 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
+import {ERC20Permit, IERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IERC20, ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DFP is ERC20, Ownable {
+contract DFP is ERC20, ERC20Permit, Ownable {
     using SafeERC20 for IERC20;
 
     uint256 private constant _CAP = 100_000_000e18;
     IERC20 private immutable _paymentToken;
     address private immutable _wallet;
 
-    constructor(IERC20 paymentToken, address wallet) ERC20("DFP", "DFP") {
+    constructor(IERC20 paymentToken, address wallet) ERC20("DFP", "DFP") ERC20Permit("DFP") {
         _paymentToken = paymentToken;
         _wallet = wallet;
 
         _mint(address(this), _CAP);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual returns (bool) {
+        return interfaceId == type(IERC20Permit).interfaceId;
     }
 
     function withdrawTokens(IERC20 token, address to, uint256 amount) external onlyOwner {
