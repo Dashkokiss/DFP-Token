34de66a04fac605cc7ba4ec234f45de4f3cce3e8
Roman Yarlykov <roman.yarlykov@metalamp.io>
2023-07-03 16:58:26 +0600

    feat: buyTokens
    Added variables:
    - _MIN_PURCHASE_AMOUNT
    - _SALE_RATE
    - _MULTIPLIER
    
    - Added MinPurchase error
    
    - Added Sold event
    
    Added functions:
    - buyTokens with amount
    - buyTokens with amount and wallet address
    - _purchaseTokens
    - getSalePrice

--- src/Dfp.sol
+++ src/Dfp.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-import {ERC20Permit, IERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {
+    ERC20Permit,
+    IERC20Permit
+} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IERC20, ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
@@ -10,8 +13,16 @@ contract DFP is ERC20, ERC20Permit, Ownable {
     using SafeERC20 for IERC20;
 
     uint256 private constant _CAP = 100_000_000e18;
+    uint256 private constant _MIN_PURCHASE_AMOUNT = 1e18;
+    uint256 private constant _SALE_RATE = 0.1e6; // 0.1 USDT
+    uint256 private constant _MULTIPLIER = 1e18;
+
     IERC20 private immutable _paymentToken;
     address private immutable _wallet;
+
+    error MinPurchase(uint256 minAmount);
+
+    event Sold(address indexed buyer, uint256 amount, uint256 price);
 
     constructor(IERC20 paymentToken, address wallet) ERC20("DFP", "DFP") ERC20Permit("DFP") {
         _paymentToken = paymentToken;
@@ -30,5 +41,31 @@ contract DFP is ERC20, ERC20Permit, Ownable {
 
     function getPaymentToken() external view returns (address) {
         return address(_paymentToken);
+    }
+
+    function getSalePrice(uint256 purchaseAmount) public pure returns (uint256 salePrice) {
+        return (purchaseAmount * _SALE_RATE) / _MULTIPLIER;
+    }
+
+    function buyTokens(uint256 purchaseAmount) external {
+        _purchaseTokens(purchaseAmount, _wallet);
+    }
+
+    function buyTokens(uint256 purchaseAmount, address wallet) external {
+        _purchaseTokens(purchaseAmount, wallet);
+    }
+
+    function _purchaseTokens(uint256 purchaseAmount, address wallet) private {
+        if (purchaseAmount < _MIN_PURCHASE_AMOUNT) {
+            revert MinPurchase(_MIN_PURCHASE_AMOUNT);
+        }
+
+        uint256 purchasePrice = getSalePrice(purchaseAmount);
+        assert(purchasePrice > 0);
+
+        _paymentToken.safeTransferFrom(msg.sender, wallet, purchasePrice);
+        _transfer(address(this), msg.sender, purchaseAmount);
+
+        emit Sold(msg.sender, purchaseAmount, purchasePrice);
     }
 }
