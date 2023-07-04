15ce79c79b9d507816696dbfb2f4f61181292dd9
Roman Yarlykov <roman.yarlykov@metalamp.io>
2023-07-04 11:24:01 +0600

    refactor:
    Renamed:
    - from _MIN_PURCHASE_AMOUNT to MIN_PURCHASE_AMOUNT
    - from _SALE_RATE to SALE_RATE
    
    Changed visibility:
    - MIN_PURCHASE_AMOUNT from private to public
    - SALE_RATE from private to public
    
    Removed:
    - getSalePrice function
    - invariant check in _purchaseTokens

--- src/Dfp.sol
+++ src/Dfp.sol
@@ -13,9 +13,10 @@ contract DFP is ERC20, ERC20Permit, Ownable {
     using SafeERC20 for IERC20;
 
     uint256 private constant _CAP = 100_000_000e18;
-    uint256 private constant _MIN_PURCHASE_AMOUNT = 1e18;
-    uint256 private constant _SALE_RATE = 0.1e6; // 0.1 USDT
     uint256 private constant _MULTIPLIER = 1e18;
+
+    uint256 public constant MIN_PURCHASE_AMOUNT = 1e18;
+    uint256 public constant SALE_RATE = 0.1e6; // 0.1 USDT
 
     IERC20 private immutable _paymentToken;
     address private immutable _wallet;
@@ -43,10 +44,6 @@ contract DFP is ERC20, ERC20Permit, Ownable {
         return address(_paymentToken);
     }
 
-    function getSalePrice(uint256 purchaseAmount) public pure returns (uint256 salePrice) {
-        return (purchaseAmount * _SALE_RATE) / _MULTIPLIER;
-    }
-
     function buyTokens(uint256 purchaseAmount) external {
         _purchaseTokens(purchaseAmount, _wallet);
     }
@@ -56,12 +53,11 @@ contract DFP is ERC20, ERC20Permit, Ownable {
     }
 
     function _purchaseTokens(uint256 purchaseAmount, address wallet) private {
-        if (purchaseAmount < _MIN_PURCHASE_AMOUNT) {
-            revert MinPurchase(_MIN_PURCHASE_AMOUNT);
+        if (purchaseAmount < MIN_PURCHASE_AMOUNT) {
+            revert MinPurchase(MIN_PURCHASE_AMOUNT);
         }
 
-        uint256 purchasePrice = getSalePrice(purchaseAmount);
-        assert(purchasePrice > 0);
+        uint256 purchasePrice = (purchaseAmount * SALE_RATE) / _MULTIPLIER;
 
         _paymentToken.safeTransferFrom(msg.sender, wallet, purchasePrice);
         _transfer(address(this), msg.sender, purchaseAmount);
